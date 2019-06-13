c76566a7c1a2036d61ee214ac00cb1f717ca7070
Glynnis Thomas <glynnis.thomas@kdol.ks.gov>
2019-06-13 15:03:02 -0500

    pushing practice files for wfh

---
+++ AspNetCoreConfigurationBasics/WelcomeConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreConfigurationBasics
+{
+    public class WelcomeConfig
+    {
+        public string Message { get; set; }
+
+        public int TodaysNumber { get; set; }
+
+        public bool SystemUp { get; set; }
+    }
+}
